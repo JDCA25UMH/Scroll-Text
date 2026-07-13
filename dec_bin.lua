fb250596fd80541b55d955d27391de9a2730e201
JDCA25UMH <castroariasjuandavid369@gmail.com>
2026-07-12 21:28:44 -0500

    Refactor decimal to binary conversion function

--- dec_bin.lua
+++ dec_bin.lua
@@ -1,26 +1,21 @@
 decimal = 12
 
-function dec_bin_div2(decimal)
+function dec_bin(decimal)
 local i = 1   
 local accum = {}
 local bin = {}
 while decimal >= 1 do   
-   
         dec_bin = decimal % 2
    decimal = math.floor(decimal / 2)
-
-   
-
-   accum[i] = dec_bin
-  
-   i = i +1
+     accum[i] = dec_bin
+     i = i +1
 end
 a= 1
 for i=#accum,1,-1 do
  bin[i] = accum[a]
 a= a+1 
 end
-return table.unpack(bin)
+return table.concat(bin)
 end
 
-print(dec_bin_div2(decimal))
+print(dec_bin(decimal))
